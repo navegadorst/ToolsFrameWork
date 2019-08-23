a12cc540986c5389c111a983f42ba7b3e73b4583
Alex Torres <alejandro.torres.e@gmail.com>
2019-08-23 00:08:50 -0500

    integración de herramienta para conexion api y ejecutar post con un certificado

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ToolsFrameWork" 
---
+++ ToolsFrameWork/Tools/ApiWithCert.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsFrameWork.Tools
+{
+    public class ApiWithCert
+    {
+        public ApiWithCert()
+        {
+
+        }
+        /// <summary>
+        /// Metodo para poder enviar peticion POST a API
+        /// Esta petición implementa el uso de certificado 
+        /// </summary>
+        public void ApiPost()
+        {
+            // url de servicio
+            string urlApi = "";
+            //data en este caso json
+            string strContent = "{'prop':'datavalue'}";
+            //var stringContent = new StringContent(strContent);
+            //segmento donde se prepara el contenido o data para adjuntarlo en la petición post
+            var stringContent = new StringContent(strContent, Encoding.UTF8, "application/json");
+            //apartado donde se lee y carga el certificado en x5092
+            X509Certificate2 clientCert = new X509Certificate2("");
+            //segmento para adjuntar el certificado en handler
+            WebRequestHandler requestHandler = new WebRequestHandler();
+            requestHandler.ClientCertificates.Add(clientCert);
+
+            //using para carga de httpclient y ejecución de petición a api  
+            //se adjunta el certificado por medio del handler en el constructor
+            using (HttpClient client = new HttpClient(requestHandler))
+            {
+                //se ejecuta la petición post a api
+                HttpResponseMessage response = client.PostAsync(urlApi, stringContent).Result;
+                response.EnsureSuccessStatusCode();
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+
+
+            }
+        }
+    }
+}
